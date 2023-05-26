288225cb2c8354ec88b4d6b80764f5dbac0a262a
Mohan-Vinotharamkumar <navaneethamohan2002@gmail.com>
2023-05-26 23:31:32 +0530

    first commit

---
+++ handling.js
@@ -0,0 +1,12 @@
+function call_wp()
+{
+    var mob_number = +918220473852;
+    var url = 'https://api.whatsapp.com/send?phone=' 
+    + mob_number + '&text= Hi, I want to Purchase Jewels.';
+    window.open(url,'_blank');
+}
+
+function call_insta()
+{
+    window.open("https://www.instagram.com/elegant_jewels_for_her/?next=%2F",'_blank')
+}
